ef5580e82ee22b3ba4ffd1afb56c2ac2ae2af1f8
“ <weiliangze@vip.qq.com>
2017-07-15 22:53:55 +0800

    update Practice 6-2.py

--- Chapter6/Practice 6-2.py
+++ Chapter6/Practice 6-2.py
@@ -5,4 +5,4 @@ street = easygui.enterbox ("Which street do you live in?")
 city = easygui.enterbox ("Which city do you come from?")
 postcode = easygui.enterbox ("What's your postcode?")
 
-easygui.msgbox( "Here is your dress:\n "+ name +"\n"+ street + "\n"+ city + "\n"+ postcode + "\n")
+easygui.msgbox( "Here is your dress:\n"+ name +"\n"+ street + "\n"+ city + "\n"+ postcode + "\n")
