9cb9ad254e925252ac90a75f8c1b13b36da32351
“ <weiliangze@vip.qq.com>
2017-07-15 20:16:58 +0800

    upload Chapter3

---
+++ Chapter3/Practice 3-1b.py
@@ -0,0 +1,4 @@
+perimeter = ( 12.5 + 16.7 ) * 2
+area = 12.5 * 16.7
+print "The perimeter of this room is",perimeter
+print "The area of this room is",area
---
+++ Chapter3/Practice 3-2.py
@@ -0,0 +1,3 @@
+Fahrenheit = 77
+Celsius = 5.0 / 9 * (Fahrenheit - 32)
+print Celsius
