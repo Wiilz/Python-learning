43157a7f4e44f15ab0ecd58bbc5f33732a2ae84f
Wiilz <rayleignze@gmail.com>
2018-03-08 22:44:53 +0800

    Upload Chapter15

---
+++ Chapter15/Code list 15-3.py
@@ -0,0 +1,8 @@
+from time import sleep
+print "How",
+sleep(2)
+print "are",
+sleep(2)
+print "you",
+sleep(2)
+print "today?"
---
+++ Chapter15/Practice 15-3.py
@@ -0,0 +1,6 @@
+from random import randint
+
+randomList = []
+for i in range(5):
+    randomList.append(randint(1,21))
+print randomList
