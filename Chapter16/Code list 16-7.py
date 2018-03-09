f71b3ee7e0acfc54514be3cefde5326d02535d13
Wiilz <rayleignze@gmail.com>
2018-03-09 23:51:05 +0800

    Upload Chapter16

---
+++ Chapter16/Code list 16-7.py
@@ -0,0 +1,16 @@
+﻿import pygame, sys, math
+
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255,255,255])   #用白色背景填充窗口
+for x in range(0, 640):      #从左到右循环(x= 0到639)
+    y = int(math.sin(x/640.0 * 4 * math.pi) * 200 + 240)   #计算每个点的y坐标（垂直坐标）
+
+    pygame.draw.rect(screen, [0,0,0], [x, y, 1, 1], 1)  #使用小矩形来画点
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
---
+++ Chapter16/Code list 16-8.py
@@ -0,0 +1,18 @@
+﻿import pygame, sys, math
+
+pygame.init()
+screen = pygame.display.set_mode([640, 480])
+screen.fill([255,255,255])   #用白色背景填充窗口
+plotPoints = []
+
+for x in range(0, 640):      #从左到右循环(x= 0到639)
+    y = int(math.sin(x/640.0 * 4 * math.pi) * 200 + 240)   #计算每个点的y坐标（垂直坐标）
+    plotPoints.append([x,y])    #要连接点的列表
+pygame.draw.lines(screen, [0,0,0], False, plotPoints, 2)  
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
