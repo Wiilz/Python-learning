b6503e76b1f506ae3da7f30bddc1946e814eb6c0
Wiilz <rayleignze@gmail.com>
2018-03-20 22:54:46 +0800

    Upload CodeList 19-1

---
+++ Chapter19/Code list 19-1.py
@@ -0,0 +1,20 @@
+﻿import pygame, sys
+#初始化Pygame和mixer
+pygame.init()
+pygame.mixer.init()
+
+#创建一个Pygame窗口
+screen = pygame.display.set_mode([640,480])
+#等一秒钟让mixer完成初始化
+pygame.time.delay(1000)
+
+#创建声音对象
+splat = pygame.mixer.Sound("splat.wav")
+splat.play()
+
+running = True
+while running :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
