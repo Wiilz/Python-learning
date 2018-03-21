a6515864a7b616e6b666a07255ca254225ea6e93
Wiilz <rayleignze@gmail.com>
2018-03-21 23:32:34 +0800

    Upload CodeList 19-2,3,4

---
+++ Chapter19/Code list 19-2.py
@@ -0,0 +1,19 @@
+﻿import pygame, sys
+#初始化Pygame和mixer
+pygame.init()
+pygame.mixer.init()
+
+#创建一个Pygame窗口
+screen = pygame.display.set_mode([640,480])
+#等一秒钟让mixer完成初始化
+pygame.time.delay(1000)
+
+pygame.mixer.music.load("bg_music.mp3")
+pygame.mixer.music.play()
+
+running = True
+while running :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
---
+++ Chapter19/Code list 19-4.py
@@ -0,0 +1,26 @@
+﻿import pygame, sys
+#初始化Pygame和mixer
+pygame.init()
+pygame.mixer.init()
+
+#创建一个Pygame窗口
+screen = pygame.display.set_mode([640,480])
+#等一秒钟让mixer完成初始化
+pygame.time.delay(1000)
+
+pygame.mixer.music.load("bg_music.mp3")
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play()
+
+splat = pygame.mixer.Sound("splat.wav")
+splat.set_volume(0.50)
+
+running = True
+while running :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if not pygame.mixer.music.get_busy():
+        splat.play()
+        pygame.time.delay(1000)
+pygame.quit()
