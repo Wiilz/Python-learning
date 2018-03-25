c59f7e4756efaad1b9644410b235b0aefaf3c095
Wiilz <rayleignze@gmail.com>
2018-03-25 22:36:21 +0800

    Upload CodeList 20-1

---
+++ Chapter20/Code list 20-1.py
@@ -0,0 +1,14 @@
+﻿import sys
+from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
+
+from_class = uic.loadUiType("MyFirstGui.ui")[0]   #加载在Designer中创建的UI
+
+class MyWindowClass(QtGui.QMainWindow, from_class):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+
+app = QtGui.QApplication(sys.argv)
+myWindow = MyWindowClass()
+myWindow.show()
+app.exec_()
