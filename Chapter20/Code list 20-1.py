04e39e02b5fb84f773c05743dc007b140c19115b
Wiilz <rayleignze@gmail.com>
2018-03-29 23:53:54 +0800

    Update CL20-1

--- Chapter20/Code list 20-1.py
+++ Chapter20/Code list 20-1.py
@@ -1,8 +1,8 @@
 ﻿import sys
-#from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
+# from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
 import PyQt4
 
-from_class = uic.loadUiType("MyFirstGui.ui")[0]   #加载在Designer中创建的UI
+from_class = PyQt4.uic.loadUiType("MyFirstGui.ui")[0]   #加载在Designer中创建的UI
 
 class MyWindowClass(QtGui.QMainWindow, from_class):
     def __init__(self, parent=None):
