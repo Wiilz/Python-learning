ef38eb6260087ed43c50294253ffdb00400937ac
Wiilz <rayleignze@gmail.com>
2018-03-30 23:05:36 +0800

    Upload Code List 20-2

--- Chapter20/Code list 20-2.py
+++ Chapter20/Code list 20-2.py
@@ -7,6 +7,13 @@ class MyWindowClass(QtGui.QMainWindow, from_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        self.pushButton.clicked.connect(self.button_clicked) #将事件处理器与事件相连接
+    def button_clicked(self):
+        x = self.pushButton.x()
+        y = self.pushButton.y()
+        x += 50
+        y += 50
+        self.pushButton.move(x,y)
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass()
