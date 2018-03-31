a9b80760c2d3a648d5b9c435afeecdaed041d8d5
Wiilz <rayleignze@gmail.com>
2018-03-31 23:36:34 +0800

    Fix the bug & Upload Code list 20-3

--- Chapter20/Code list 20-2.py
+++ Chapter20/Code list 20-2.py
@@ -1,9 +1,9 @@
 ﻿import sys
 from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
 
-from_class = uic.loadUiType("MyFirstGui.ui")[0]   #加载在Designer中创建的UI
+form_class = uic.loadUiType("MyFirstGui.ui")[0]   #加载在Designer中创建的UI
 
-class MyWindowClass(QtGui.QMainWindow, from_class):
+class MyWindowClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
---
+++ Chapter20/TempGui.py
@@ -0,0 +1,24 @@
+﻿import sys
+from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
+
+form_class = uic.loadUiType("tempconv.ui")[0]   #加载UI定义
+
+class MyWindowClass(QtGui.QMainWindow, form_class):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        self.btn_CtoF.clicked.connect(self.btn_CtoF_clicked) #将事件处理器与事件相连接
+        self.btn_FtoC.clicked.connect(self.btn_FtoC_clicked)
+    def btn_CtoF_clicked(self):    #CtoF按钮的事件处理器
+        cel = float(self.editCel.text())
+        fahr = cel * 9 / 5.0 + 32
+        self.spinFahr.setValue(int(fahr + 0.5))        
+    def btn_FtoC_clicked(self):
+        fahr = self.spinFahr.value()
+        cel = (fahr - 32)*5 / 9.0
+        self.editCel.setText(str(cel))
+
+app = QtGui.QApplication(sys.argv)
+myWindow = MyWindowClass(None)
+myWindow.show()
+app.exec_()
