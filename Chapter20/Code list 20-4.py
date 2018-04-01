8a584f22287bd18795c479a7398590a2d4dfcf02
Wiilz <rayleignze@gmail.com>
2018-04-01 23:33:21 +0800

    Upload Code List 20-4

--- Chapter20/Code list 20-4.py
+++ Chapter20/Code list 20-4.py
@@ -1,7 +1,7 @@
 ﻿import sys
 from PyQt4 import QtCore, QtGui, uic     #导入所需的PyQt库
 
-form_class = uic.loadUiType("tempconv.ui")[0]   #加载UI定义
+form_class = uic.loadUiType("tempconv_menu.ui")[0]   #加载UI定义
 
 class MyWindowClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
@@ -9,6 +9,9 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
         self.setupUi(self)
         self.btn_CtoF.clicked.connect(self.btn_CtoF_clicked) #将事件处理器与事件相连接
         self.btn_FtoC.clicked.connect(self.btn_FtoC_clicked)
+        self.actionC_to_F.triggered.connect(self.btn_CtoF_clicked)
+        self.actionF_to_C.triggered.connect(self.btn_FtoC_clicked)
+        self.actionExit.triggered.connect(self.menuExit_selected)
 
     def btn_CtoF_clicked(self):    #CtoF按钮的事件处理器
         cel = float(self.editCel.text())
@@ -19,6 +22,8 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
         fahr = self.spinFahr.value()
         cel = (fahr - 32)*5 / 9.0
         self.editCel.setText(str(cel))
+    def menuExit_selected(self):
+        self.close()
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass(None)
